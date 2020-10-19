411a52c7207130c6cba46a92c7b46e5e35b3a513
fridtjbs <71632344+fridtjbs@users.noreply.github.com>
2020-10-19 13:52:38 +0200

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#Setting variables
+# Setter variables
 IPADDR="$1"
 
-#BUgfix
+# Sjekker at en IP-adresse følger med 
 if [[ -z "$IPADDR" ]]; then
 	echo "usage: $0 <ip adresse>" 1>&2
 	exit
 fi
 
+# Banner en IP-adressen 
 echo "Banning $IPADDR"
 sudo iptables -A INPUT -s "$IPADDR" -j DROP
+
+# Sender IP-adressen til miniban.db
 echo $IPADDR,$(date +%s) >> miniban.db
 
 
